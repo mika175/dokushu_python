d0fe9b505668b74e70a8a0939c4221ab58dd2af3
mika175 <97383182+mika175@users.noreply.github.com>
2022-01-12 17:59:42 +0900

    Update basic_grammer.py

---
+++ basic_grammer.py
@@ -0,0 +1,31 @@
+#First of all...
+print("Hello world!") 
+
+#This is called a comment. You can use "comment out" to leave a message for other programmers.
+
+#let's make some variable and play with them!
+#Python makes things easy. You can just claim variables without extra signs such as $ or var...beautiful, isn't it?
+a = 5000
+b = 2000
+
+#basic number handling
+print(a + b) 
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) 
+
+#knowing the data type is important.
+print(type(a))
+
+#what about this?
+c = str(a)
+print(c)
+
+#what do you think will happen?
+#print(b + c)
+
+#I would like you to add b + c. How can you do that? Write your answer below. 
+
+#b is INT & c is STR. Therefore, write as follows.
+print (int(b) + int(c))
